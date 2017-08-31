544665a11fcaebfb91ae1615a177fa6f9c6c7f6c
BOSS-PC\boss <contact@eplex.eu>
2017-08-31 12:12:53 +0200

    v4 init fnct

--- Budgety/app.js
+++ Budgety/app.js
@@ -43,20 +43,32 @@ var controller = (function(budgetCtrl, UICtrl) {
 	// console.log('odpalone');
 	// console.log(budgetCtrl + ' ' + UICtrl);
 
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+		document.addEventListener('keypress', function(event) {
+			if(event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
+
+	
 
 	var ctrlAddItem = function() {
 
 		var input = UICtrl.getInput();
-
 		console.log(input);
-	}
+	};
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-	document.addEventListener('keypress', function(event) {
-		if(event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function () {
+			console.log('app start');
+			setupEventListeners();
 		}
-	});
+	};
 
 })(budgetController, UIController);
+
+controller.init();
