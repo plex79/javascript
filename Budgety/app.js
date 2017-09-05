ec3ae0078e084689e7af59a3d182c7fff3b2251a
BOSS-PC\boss <contact@eplex.eu>
2017-09-05 13:48:49 +0200

    v5

--- Budgety/app.js
+++ Budgety/app.js
@@ -75,7 +75,7 @@ var UIController = (function() {
 			return {
 				type: document.querySelector(DOMstrings.inputType).value,
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inputValue).value
+				value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
 			};
 		},
 
@@ -139,19 +139,31 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 	};
 
+    var updateBudget = function() {
+        
+    };
+    
 	var ctrlAddItem = function() {
 
 		var input, newItem;
 
 		input = UICtrl.getInput();
 		console.log(input);
-
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-		UICtrl.addListItem(newItem, input.type);
         
-        UICtrl.clearFields();
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
+            UICtrl.addListItem(newItem, input.type);
+
+            UICtrl.clearFields();
+
+            updateBudget();
+            
+        }
+
+		
+        
 	};
 
 	return {
