5a0fa218025dc62c48557b5a4cc7619328297aed
BOSS-PC\boss <contact@eplex.eu>
2017-09-11 09:00:44 +0200

    v7

--- Budgety/app.js
+++ Budgety/app.js
@@ -104,7 +104,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -152,7 +156,16 @@ var UIController = (function() {
             fieldsArr[0].focus();
             
         },
-
+        
+        displayBudget: function(obj) {
+            
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+            
+        },
+        
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -181,6 +194,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         budgetCtrl.calculateBudget();
         
         var budget = budgetCtrl.getBudget();
+        
+        UICtrl.displayBudget(budget);
         
         console.log(budget);
         
